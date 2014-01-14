3ae24675c5a17cdaea877a060a95f86cee7750ef
reaperjc <reaperjc@gmail.com>
2014-01-14 12:37:02 -0300

    Todos los Fixtures estan en la carpeta FixturesReady en los DataFixtures. Copien las fixtures que les falten en la carpeta ORM y luego corran symfony doctrine:fixtures:load

--- src/LFP/StructuralAnks/MainBundle/Entity/Residue.php
+++ src/LFP/StructuralAnks/MainBundle/Entity/Residue.php
@@ -89,9 +89,9 @@ class Residue
      *  @ORM\ManyToOne(targetEntity="Chain", inversedBy="residues")
      */
     protected $chain;
-
+    
     /**
-     *  @ORM\OneToOne(targetEntity="CharactPos", mappedBy="residue")
+     *  @ORM\OneToMany(targetEntity="CharactPos", mappedBy="residue")
      */
     protected $charactPos;
     
@@ -336,22 +336,41 @@ class Residue
     }
 
     /**
-     * Set charactPos
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->charactPos = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Add charactPos
      *
      * @param \LFP\StructuralAnks\MainBundle\Entity\CharactPos $charactPos
      * @return Residue
      */
-    public function setCharactPos(\LFP\StructuralAnks\MainBundle\Entity\CharactPos $charactPos = null)
+    public function addCharactPos(\LFP\StructuralAnks\MainBundle\Entity\CharactPos $charactPos =null)
     {
-        $this->charactPos = $charactPos;
-    
+        if($charactPos){
+            $this->charactPos[] = $charactPos;
+        }
         return $this;
+    }
+
+    /**
+     * Remove charactPos
+     *
+     * @param \LFP\StructuralAnks\MainBundle\Entity\CharactPos $charactPos
+     */
+    public function removeCharactPos(\LFP\StructuralAnks\MainBundle\Entity\CharactPos $charactPos)
+    {
+        $this->charactPos->removeElement($charactPos);
     }
 
     /**
      * Get charactPos
      *
-     * @return \LFP\StructuralAnks\MainBundle\Entity\CharactPos 
+     * @return \Doctrine\Common\Collections\Collection 
      */
     public function getCharactPos()
     {
