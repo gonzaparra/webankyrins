1c5d0afa2b00f841770509935b7c302279bc75cc
DarkSearinoX <ferodriguez.mbl@gmail.com>
2014-01-15 21:45:55 -0300

    browse for chains showing

--- src/LFP/StructuralAnks/MainBundle/Resources/public/js/main.js
+++ src/LFP/StructuralAnks/MainBundle/Resources/public/js/main.js
@@ -5,11 +5,32 @@
  */
 
 $(document).ready(function() {
+    
     $("#toggle-button").click(function(){
         $("#toggle-container").toggle();
     });
+    
+    /*When page loads, show the corresponding div*/
+    if($('#selection').val()==1){
+        $("#structures").show();
+        $("#chains").hide();
+    }
+    if($('#selection').val()==2){
+        $("#structures").hide();
+        $("#chains").show();
+    }
+    $("#selection").change(function(){
+        if($('#selection').val()==1){
+            $("#structures").show('fade');
+            $("#chains").hide('fade');
+        }
+        if($('#selection').val()==2){
+            $("#structures").hide('fade');
+            $("#chains").show('fade');
+        }
+    });
     $(function() {
-        $( "#browser-accordion" ).accordion({
+        $( "#browser-accordion-structures, #browser-accordion-chains" ).accordion({
             collapsible:true,
             active:false,
             heightStyle: "content"
